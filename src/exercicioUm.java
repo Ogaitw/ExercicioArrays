7ad45f66a994a4dcd9d6163c6e716a6170951606
Ogaitw <tiagoaugusto2402@gmail.com>
2023-04-07 01:10:47 -0300

    Initial commit

---
+++ src/exercicioUm.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class exercicioUm {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] number = new int[10];
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("Digite o " + (i+1) +"° Numero");
+            number[i] = scanner.nextInt();
+            
+        }
+        for (int i = 9; i >= 0 ; i--) {
+            System.out.println(number[i]);
+        }
+        }
+    }
